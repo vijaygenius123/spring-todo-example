78fc874a5ded051af1526611b42a6a212fc97f77
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-10-05 22:47:21 +0530

    Initial commit

---
+++ src/main/java/com/vijaygenius123/learning/springtodoexample/SpringTodoExampleApplication.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.learning.springtodoexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTodoExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vijaygenius123/learning/springtodoexample/SpringTodoExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.learning.springtodoexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
