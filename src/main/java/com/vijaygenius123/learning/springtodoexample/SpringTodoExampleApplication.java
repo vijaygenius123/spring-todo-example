05bfa2cc363c30a9ff360d4fc5f4796e5cf68aaa
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-10-06 16:45:03 +0530

    Added Swagger

--- src/main/java/com/vijaygenius123/learning/springtodoexample/SpringTodoExampleApplication.java
+++ src/main/java/com/vijaygenius123/learning/springtodoexample/SpringTodoExampleApplication.java
@@ -2,8 +2,10 @@ package com.vijaygenius123.learning.springtodoexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringTodoExampleApplication {
 
     public static void main(String[] args) {
